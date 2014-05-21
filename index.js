34fd1f8b90e2f749a157c51223f7755bebf3f4bb
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2014-05-21 14:45:51 +0900

    Use bin-wrapper v0.3.4
    Largely thanks to @kevva

---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var BinBuild = require('bin-build');
+var BinWrapper = require('bin-wrapper');
+var chalk = require('chalk');
+var fs = require('fs');
+var path = require('path');
+
+/**
+ * Initialize a new BinWrapper
+ */
+
+var bin = new BinWrapper()
+
+  .src('https://raw.github.com/1000ch/node-jpeg-recompress-bin/master/vendor/osx/jpeg-recompress', 'darwin')
+  .src('https://raw.github.com/1000ch/node-jpeg-recompress-bin/master/vendor/linux/jpeg-recompress', 'linux')
+  .src('https://raw.github.com/1000ch/node-jpeg-recompress-bin/master/vendor/win/jpeg-recompress.exe', 'win32')
+  .dest(path.join(__dirname, 'vendor'))
+  .use('jpeg-recompress');
+
+/**
+* Only run check if binary doesn't already exist
+*/
+
+fs.exists(bin.use(), function (exists) {
+  if (!exists) {
+    bin.run(['--version'], function (err) {
+      if (err) {
+        console.log(chalk.red('✗ pre-build test failed, compiling from source...'));
+
+        var builder = new BinBuild()
+          .src('https://github.com/danielgtaylor/jpeg-archive/archive/1.0.1.zip')
+          .make('make && mkdir -p ' + bin.dest() + ' && mv ./jpeg-recompress ' + bin.use());
+
+        return builder.build(function (err) {
+          if (err) {
+            return console.log(chalk.red('✗ ' + err));
+          }
+
+          console.log(chalk.green('✓ jpeg-recompress built successfully'));
+        });
+      }
+
+      console.log(chalk.green('✓ pre-build test passed successfully'));
+    });
+  }
+});
+
+/**
+* Module exports
+*/
+
+module.exports.path = bin.use();
---
+++ test/test.js
@@ -0,0 +1,60 @@
+/*global afterEach, beforeEach, describe, it */
+'use strict';
+
+var assert = require('assert');
+var binCheck = require('bin-check');
+var BinBuild = require('bin-build');
+var execFile = require('child_process').execFile;
+var fs = require('fs');
+var path = require('path');
+var rm = require('rimraf');
+
+describe('jpegRecompress()', function () {
+  afterEach(function (cb) {
+    rm(path.join(__dirname, 'tmp'), cb);
+  });
+
+  beforeEach(function () {
+    fs.mkdirSync(path.join(__dirname, 'tmp'));
+  });
+
+  it('should rebuild the jpeg-recompress binaries', function (cb) {
+    var tmp = path.join(__dirname, 'tmp');
+    var builder = new BinBuild()
+      .src('https://github.com/danielgtaylor/jpeg-archive/archive/1.0.1.zip')
+      .make('make && mv ./jpeg-recompress ' + path.join(tmp, 'jpeg-recompress'));
+
+    builder.build(function (err) {
+      assert(!err);
+      assert(fs.existsSync(path.join(tmp, 'jpeg-recompress')));
+      cb();
+    });
+  });
+
+  it('should return path to binary and verify that it is working', function (cb) {
+    var binPath = require('../').path;
+
+    binCheck(binPath, ['--version'], function (err, works) {
+      cb(assert.equal(works, true));
+    });
+  });
+
+  it('should minify a JPEG', function (cb) {
+    var binPath = require('../').path;
+    var args = [
+      '--quality',
+      'high',
+      '--min',
+      '60',
+      path.join(__dirname, 'fixtures/test.jpg'),
+      path.join(__dirname, 'tmp/test.jpg')
+    ];
+
+    execFile(binPath, args, function () {
+      var src = fs.statSync(path.join(__dirname, 'fixtures/test.jpg')).size;
+      var dest = fs.statSync(path.join(__dirname, 'tmp/test.jpg')).size;
+
+      cb(assert(dest < src));
+    });
+  });
+});
